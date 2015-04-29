8ee56dfd0931339ad91e501cfaa76c021eef19e5
Frank Johnson & Jeanette Fairless <student@epicodus.com>
2015-04-29 11:39:53 -0700

    adding second integration test which passes

--- spec/vehicle_integration_spec.rb
+++ spec/vehicle_integration_spec.rb
@@ -5,9 +5,15 @@ set(:show_exceptions, false)
 
 
 describe('path for vehicle index page', {:type => :feature}) do
-  it('user clicks on a link for either vehicle list or add new vehicle') do
-  visit('/')
-  click_link('See Vehicle List')
-  expect(page).to have_content('Vehicles')
+  it('user clicks on a link for vehicle list') do
+    visit('/')
+    click_link('See Vehicle List')
+    expect(page).to have_content('Vehicles')
+  end
+
+  it('user clicks on a link for add new vehicle') do
+    visit('/')
+    click_link('Add New Vehicle')
+    expect(page).to have_content('Add a vehicle')
   end
 end
