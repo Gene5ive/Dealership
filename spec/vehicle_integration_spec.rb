35b5939f43b06be230a21c46908baed56f800559
Frank Johnson & Jeanette Fairless <student@epicodus.com>
2015-04-29 11:51:46 -0700

    adding third integration test which passes

--- spec/vehicle_integration_spec.rb
+++ spec/vehicle_integration_spec.rb
@@ -17,3 +17,14 @@ describe('path for vehicle index page', {:type => :feature}) do
     expect(page).to have_content('Add a vehicle')
   end
 end
+
+describe('path for add a new vehicle page', {:type => :feature}) do
+  it("user clicks on submit to add new vehicle") do
+    visit('/vehicles/new')
+    fill_in('make', with: "Tesla")
+    fill_in('model', with: "ModelS")
+    fill_in('year', with: "2012")
+    click_button('Add vehicle')
+    expect(page).to have_content('Success!')
+  end
+end
